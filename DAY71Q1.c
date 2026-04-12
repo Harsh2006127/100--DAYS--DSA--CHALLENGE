f79035eb9a3b4e830053c0f58bf7597678170e5f
Harsh2006127 <kohliharsh2006@gmail.com>
2026-04-12 08:29:30 +0530

    Add files via upload

---
+++ DAY71Q1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 100
+
+int table[SIZE];
+int m;
+
+// Initialize table
+void init() {
+    for (int i = 0; i < m; i++)
+        table[i] = -1;  // -1 means empty
+}
+
+// Insert using quadratic probing
+void insert(int key) {
+    int h = key % m;
+
+    for (int i = 0; i < m; i++) {
+        int index = (h + i * i) % m;
+
+        if (table[index] == -1) {
+            table[index] = key;
+            return;
+        }
+    }
+
+    printf("Hash table full\n");
+}
+
+// Search using quadratic probing
+void search(int key) {
+    int h = key % m;
+
+    for (int i = 0; i < m; i++) {
+        int index = (h + i * i) % m;
+
+        if (table[index] == key) {
+            printf("FOUND\n");
+            return;
+        }
+
+        if (table[index] == -1) {
+            break;  // stop early
+        }
+    }
+
+    printf("NOT FOUND\n");
+}
+
+int main() {
+    int q;
+    char op[10];
+    int key;
+
+    scanf("%d", &m);   // table size
+    scanf("%d", &q);   // number of operations
+
+    init();
+
+    for (int i = 0; i < q; i++) {
+        scanf("%s %d", op, &key);
+
+        if (strcmp(op, "INSERT") == 0) {
+            insert(key);
+        } else if (strcmp(op, "SEARCH") == 0) {
+            search(key);
+        }
+    }
+
+    return 0;
+}
